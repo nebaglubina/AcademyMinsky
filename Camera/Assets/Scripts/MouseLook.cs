1e52b0c32e11684a994f274d35b467f40f481898
romanlu <romanlu@playtika.com>
2020-10-14 17:06:45 +0300

    current

---
+++ Camera/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour
+{
+    [SerializeField] private float _sensitivity = 1.0f;
+
+    void Update()
+    {
+        //var mouseX = Input.GetAxis("Mouse X");
+        //var newRotation = transform.localEulerAngles;
+        //newRotation.y += _sensitivity * mouseX;
+        //transform.localEulerAngles = newRotation;
+    
+        var mouseY = Input.GetAxis("Mouse Y");
+        Vector3 newRotation1 = transform.localEulerAngles;
+        newRotation.x -= mouseY;
+        Debug.Log(newRotation.x);
+        transform.localEulerAngles = newRotation;
+    }
+}
