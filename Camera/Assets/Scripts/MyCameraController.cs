00d4c87e3ab24ddc4a66d1ed1c5427c124bfc0c1
romanlu <romanlu@playtika.com>
2020-10-14 14:37:20 +0300

    Initial

---
+++ Camera/Assets/Scripts/MyCameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera myCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Camera/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveX = Input.GetAxis("Horizontal");
+        var moveZ = Input.GetAxis("Vertical");
+        var direction = new Vector3(moveX, 0, moveZ);
+        Move(direction);
+    }
+
+    private void Move (Vector3 direction)
+    {
+        transform.Translate(direction * _moveSpeed * Time.deltaTime);
+    }
+}
