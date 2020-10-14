cdb54468ab4d9d435a19242a21235170aa172ce4
romanlu <romanlu@playtika.com>
2020-10-14 15:20:07 +0300

    Mouse rotation

--- Camera/Assets/Scripts/PlayerMovement.cs
+++ Camera/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _rotatespeed;
+    [SerializeField] private Camera _myCamera;
     
     // Start is called before the first frame update
     void Start()
@@ -17,12 +19,27 @@ public class PlayerMovement : MonoBehaviour
     {
         var moveX = Input.GetAxis("Horizontal");
         var moveZ = Input.GetAxis("Vertical");
-        var direction = new Vector3(moveX, 0, moveZ);
-        Move(direction);
+        var moveDirection = new Vector3(moveX, 0, moveZ);
+
+        var rotateX = Input.GetAxis("Mouse X");
+        var rotateY = Input.GetAxis("Mouse Y");
+        var rotateXDirection = new Vector3(0, rotateX, 0);
+        var rotateYDirection = new Vector3(-rotateY, 0, 0);
+        
+        
+
+        Move(moveDirection);
+        Rotate(rotateXDirection, rotateYDirection);
     }
 
     private void Move (Vector3 direction)
     {
         transform.Translate(direction * _moveSpeed * Time.deltaTime);
+    }
+
+    private void Rotate(Vector3 directionX, Vector3 directionY)
+    {
+        transform.Rotate(directionX * _rotatespeed * Time.deltaTime);
+        _myCamera.transform.Rotate(directionY * _rotatespeed * Time.deltaTime);
     }
 }
