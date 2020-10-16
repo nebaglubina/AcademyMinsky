867eae873f76df2ee3496b2cda2f5e2c9c501896
romanlu <romanlu@playtika.com>
2020-10-16 13:43:59 +0300

    Quick fix

---
+++ TweenPractice/Assets/Scripts/RotatingControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class RotatingControl : MonoBehaviour
+{
+    [SerializeField] private Transform point;
+    [SerializeField] private Vector3 rotationVector;
+
+    private void Start()
+    {
+        Loading();
+    }
+
+    public void Loading()
+    {
+        point.DORotate(rotationVector,1f,RotateMode.FastBeyond360).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+    }
+}
